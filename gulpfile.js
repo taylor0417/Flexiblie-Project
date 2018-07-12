fc145d38d04d1fa548e52a066485fe0e1e134cd6
taylor0417 <taylor57568@163.com>
2018-07-12 16:44:53 +0800

    Gulp环境搭建

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+gulp.task('html', function () {
+    gulp.src('./src/index.html')
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('css', function () {
+    gulp.src('./src/css/*')
+        .pipe(csso())
+        .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('js', function () {
+    gulp.src('./src/js/*')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('imagemin', function () {
+    gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+});
+
+gulp.task('default',['css', 'html', 'js', 'imagemin',]);
+
+gulp.task('dev', ['default'], function() {
+    browserSync.init({
+        server: "./dist",
+        port: 3000,
+        notify: false
+    });
+    gulp.watch("./src/*.html",['html']).on('change', reload);
+    gulp.watch("./src/css/*",['css']).on('change', reload);
+    gulp.watch("./src/js/*.js",['js']).on('change', reload);
+});
